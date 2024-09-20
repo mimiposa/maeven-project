edc00d9f4407006b3699d9bf8d17b6c010f95f58
CORP\a894174 <muriel.small@atos.net>
2024-09-20 21:05:21 +0200

    feat: Add name v2

--- my-app/src/main/java/com/exemple/App.java
+++ my-app/src/main/java/com/exemple/App.java
@@ -5,6 +5,6 @@ package com.exemple;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello Orbion v1!");
+        System.out.println("Hello Orbion v2!");
     }
 }
