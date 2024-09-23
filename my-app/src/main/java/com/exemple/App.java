05e88c7610115c1bb08c74e9f565fb3261ede46d
mimiposa <121938070+mimiposa@users.noreply.github.com>
2024-09-23 10:56:49 +0200

    fix: fix the tiles (#13)
    Co-authored-by: CORP\a894174 <muriel.small@atos.net>

--- my-app/src/main/java/com/exemple/App.java
+++ my-app/src/main/java/com/exemple/App.java
@@ -5,6 +5,6 @@ package com.exemple;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello Orbion v4!");
+        System.out.println("Hello Orbion v5!");
     }
 }
